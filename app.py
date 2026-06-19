5296389a5bee9db19afca39d4f31c52a096a4957
OLANIYI <olaniyinifemi@gmail.com>
2026-06-19 03:39:26 +0100

    "Added max downloads option to limit the number of videos downloaded from a playlist."

--- app.py
+++ app.py
@@ -94,8 +94,8 @@ def download_youtube_video(url, choice, is_playlist=False, download_path='downlo
         'cookiesfile': 'cookies.txt', # Path to cookies file for authenticated downloads
         'sleep_interval': 8,        # Wait AT LEAST 8 seconds between video downloads
         'max_sleep_interval': 25,   # Wait UP TO 25 seconds (Randomly picks a number between 8 and 25)
-        # 2. Cap the actual download speed
         #'limit_rate': '4M',         # Limits download speed to 3 Megabytes per second
+        'max_downloads': 20,          # Limit the number of videos to download from a playlist
     }
 
     if is_playlist:
